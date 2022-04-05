a57c9e50e2a3aad5bea864dcbcb5ceaff5e759f6
jorgesmunoz <jorgesmunoz@gmail.com>
2022-04-05 12:20:38 -0300

    Added profile viewset

---
+++ profiles_api/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """Allow user to edit their own profile"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check user ir trying to edit their own profile"""
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.id == request.user.id
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,10 +2,13 @@ from os import stat
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import (
-    status, viewsets)
+from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
 
 # Create your views here.
 
@@ -94,3 +97,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
